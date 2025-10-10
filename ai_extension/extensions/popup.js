ebb066cd1ba9baacc4fb8ceff6a1270a7cfd4fe6
lulu <lulutinganyu@gmail.com>
2025-10-11 01:18:20 +0800

    comment out popup

--- ai_extension/extensions/popup.js
+++ ai_extension/extensions/popup.js
@@ -1,30 +1,30 @@
-// popup.js
-let selectedText = "";
+// // popup.js
+// let selectedText = "";
 
-// 監聽 content.js 傳來的文字
-chrome.runtime.onMessage.addListener((message) => {
-  if (message.text) {
-    selectedText = message.text;
-    document.getElementById("selectedText").textContent = selectedText;
-  }
-});
+// // 監聽 content.js 傳來的文字
+// chrome.runtime.onMessage.addListener((message) => {
+//   if (message.text) {
+//     selectedText = message.text;
+//     document.getElementById("selectedText").textContent = selectedText;
+//   }
+// });
 
-// 按鈕送出
-document.getElementById("sendBtn").addEventListener("click", () => {
-  if (!selectedText) return alert("請先選文字");
+// // 按鈕送出
+// document.getElementById("sendBtn").addEventListener("click", () => {
+//   if (!selectedText) return alert("請先選文字");
 
-  fetch("https://two025fall-computernetwork.onrender.com/askAI", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ text: selectedText })
-  })
-  .then(res => res.json())
-  .then(data => {
-    if (data.reply) {
-      document.getElementById("result").textContent = data.reply;
-    } else {
-      console.error(data.error);
-    }
-  })
-  .catch(err => console.error(err));
-});
+//   fetch("https://two025fall-computernetwork.onrender.com/askAI", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ text: selectedText })
+//   })
+//   .then(res => res.json())
+//   .then(data => {
+//     if (data.reply) {
+//       document.getElementById("result").textContent = data.reply;
+//     } else {
+//       console.error(data.error);
+//     }
+//   })
+//   .catch(err => console.error(err));
+// });
