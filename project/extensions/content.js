e8b7ff949a9436d41f91078c79018017e6a14716
minnie1018 <Minnie94527@gmail.com>
2025-11-26 21:23:14 +0800

    Implement core keyboard tracking

--- project/extensions/content.js
+++ project/extensions/content.js
@@ -31,3 +31,27 @@ chrome.runtime.onMessage.addListener((message) => {
     }, 5000);
   }
 });
+
+//監聽打字(目前測試：gemini、hackmd、colab讀的到，google doc、word線上版讀不到)
+let keyCount = 0
+let backspaceCount = 0;
+let keyTimestamps = []; //每次按按鍵的時間
+
+chrome.storage.local.get(["keyCount", "backspaceCount", "keyTimestamps"], (data) => {//繼承之前/其他網站的行為資料
+  keyCount = data.keyCount || 0;
+  backspaceCount = data.backspaceCount || 0;
+  keyTimestamps = data.keyTimestamps || []; 
+  if(keyTimestamps.length > 0){
+      if(Date.now() - keyTimestamps.at(-1) > 14400000){ //4hr沒打字就重置(長期專注度記錄再另外放在後端)
+      keyTimestamps = [];
+      keyCount = 0;
+      backspaceCount = 0;
+    }
+  }
+  document.addEventListener("keydown", (e) =>{
+    keyCount++;
+    keyTimestamps.push(Date.now()); 
+    if (e.key === "Backspace") backspaceCount++;
+    chrome.storage.local.set({keyCount, backspaceCount, keyTimestamps});
+  });
+});
