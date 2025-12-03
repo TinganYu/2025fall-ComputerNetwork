309dc4e9fe2d62fa426b4b04cc0f097358ccef90
lapudas <lapudas940324@gmail.com>
2025-12-03 17:33:42 +0800

    database outline

---
+++ project/backend/database.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+import os
+from flask import Flask, request, jsonify
+import requests
+from flask_cors import CORS
+import psycopg2
+
+app = Flask(__name__)
+CORS(app)
+
+load_dotenv()
+DB_URL = os.environ.get("E_DB_URL")
+
+@app.route("/db_store", methods=["POST"])
+def insert():
+    with psycopg2.connect(DB_URL) as conn:
+        with conn.cursor() as cur:
+            data = request.json
+            command = "INSERT INTO your_table (column1, column2) VALUES (%s, %s)"
+            cur.execute(command, (None,None))
+            conn.commit()
+    return
+    
+@app.route("/db_get", methods=["POST"])
+def select():
+    with psycopg2.connect(DB_URL) as conn:
+        with conn.cursor() as cur:
+            data = request.json
+            command = "SELECT * FROM your_table WHERE user_id = %s"
+            cur.execute(command, (None,))
+            rows = cur.fetchall()
+    return
