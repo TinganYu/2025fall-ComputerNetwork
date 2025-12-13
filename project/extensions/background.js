ba2a95558b1a3ab3bd88f5a6ff92a548a82c954d
lapudas <lapudas940324@gmail.com>
2025-12-13 22:23:47 +0800

    add all db url

--- project/extensions/background.js
+++ project/extensions/background.js
@@ -260,8 +260,7 @@ chrome.alarms.onAlarm.addListener((alarm) => {
                 
                 // 算FocusScore S (假設 w1=0.33, w2=0.33, w3=0.34, b=0)
                 // S = w1*Delta_v' + w2*p' + w3*d' + b
-                const bias = data_sync.bias || 0; // 從 sync 讀取偏置項 (有從sync讀到嗎?待確認)
-                console.log("bias correct?",data.bias)
+                const bias = data_sync.bias || 0; // 從 sync 讀取偏置項
                 const focusScore = ((0.33 * deltaVPrime) + (0.33 * pauseTimePrime) + (0.34 * errorRatePrime)) * 100 + bias;
 
                 // 確保分數在 0 到 100 之間
