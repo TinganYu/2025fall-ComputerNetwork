18198a8024757a0ff2da7f778715ac1bccf56c8e
lapudas <lapudas940324@gmail.com>
2025-12-14 01:36:11 +0800

    add review show

--- project/backend/database.py
+++ project/backend/database.py
@@ -56,7 +56,7 @@ def show_review():
     now_ms = data.get("now", "")
     now_ms += 8 * 60 * 60 * 1000    # UTC+8
     now_ms -= 1000  # 避免延遲影響計算
-    now_day = (math.ceil(now_ms / (1000 * 60 * 30)) -1) / 48
+    now_day = math.floor((math.ceil(now_ms / (1000 * 60 * 30)) -1) / 48)
     user_id = data.get("id","")
     if not user_id or not now_ms:
         print(data)
@@ -66,17 +66,34 @@ def show_review():
         with psycopg2.connect(DB_URL) as conn:
             with conn.cursor() as cur:
                 data = request.json
-                command = f"SELECT \"Timestamps\", focus FROM your_table WHERE user_id = %s AND (\"Timestamps\"-1)/48 = {now_day}"
-                cur.execute(command, (user_id,))
+                command = "SELECT \"Timestamps\", focus FROM daily WHERE user_id = %s AND (\"Timestamps\"-1)/48 = %s"
+                cur.execute(command, (user_id,now_day))
                 dailys = cur.fetchall()
+                dailyLabels = []
+                dailyData = []
+                for i in range(48):
+                    dailyLabels.append(f"{math.floor(i / 2)}:{30*(i%2):02d}")
+                    dailyData.append(0)
+                for i in dailys:
+                    p = (i[0]-1) % 48
+                    dailyData[p] = i[1]
                 
-                command = f"SELECT \"Timestamps\", focus FROM your_table WHERE user_id = %s AND (\"Timestamps\"-1)/48 = {now_day}"
-                cur.execute(command, (user_id,))
-                dailys = cur.fetchall()
+                command = "SELECT day, focus FROM weekly WHERE user_id = %s AND %s - day < 7"
+                cur.execute(command, (user_id,now_day))
+                weeklys = cur.fetchall()    # 起始日是星期四
+                weeklyLabels = ['週日', '週一', '週二', '週三', '週四', '週五', '週六']
+                weeklyData = [0,0,0,0,0,0,0]
+                for i in weeklys:
+                    p = (i[0] + 4) % 7
+                    weeklyData[p] = i[1]
+                p_ = (now_day + 4) % 7
+                weeklyLabels[p_] = "今日"
+                for i in range(p_+1,8):
+                    weeklyLabels[i] = "上" + weeklyLabels[i]
+        return render_template('review_AI.html',dailyLabels=dailyLabels,dailyData=dailyData,weeklyLabels=weeklyLabels,weeklyData=weeklyData)
     except Exception as e:
         print("Database error:", e)
         return jsonify({"error": "Database error"}), 500
-    return
 
 @app.route("/check_id", methods=["POST"])
 def check_id():
